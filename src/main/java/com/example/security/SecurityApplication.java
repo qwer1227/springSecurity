9b2a27c427f97758f437812b077ebc8e906b9103
qwer1227 <seongmin123456@naver.com>
2025-04-14 13:20:29 +0900

    gradle 수정

--- src/main/java/com/example/security/SecurityApplication.java
+++ src/main/java/com/example/security/SecurityApplication.java
@@ -2,9 +2,15 @@ package com.example.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SecurityApplication {
+public class SecurityApplication extends SpringBootServletInitializer {
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return super.configure(application);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SecurityApplication.class, args);
